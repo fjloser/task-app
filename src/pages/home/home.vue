d3e776efac3a84fc60f8e10bd3cd1a8f8dac6a16
fangjie <1848052880@qq.com>
2021-06-01 20:12:59 +0800

    6-1

--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -6,10 +6,42 @@
 				<input type="text" v-model="searchStr" placeholder="搜素"/>
 			</view>
 		</view>
-		<Site style="margin-top: 100px;"></Site>
+		<view class="service">
+			<view class="service-list" v-for="(ele, index) in service" :key="index" :class="{active:index == serviceIndex}" @click="changeService(index)">
+				{{ele}}
+			</view>
+		</view>
+		<liFilter @change="change" @select="select" :datalist="datalist" :height="height"></liFilter>
 		<view class="content">
 			<view class="condition-box">
 				<view class="type-list" v-for="">
+					
+				</view>
+			</view>
+		</view>
+		<view class="alert-select">
+			<view class="select-type" v-for="(ele, index) in selectArr" :key='index'>
+				<view class="type-name">
+					{{ele.type}}
+				</view>
+				<view class="select-box">
+					<view class="select-list" v-for="(ele, index1) in ele.arr" :key="index1" :class="{active:index1 == select1Index[index]}" @click="changeSelect1(index, index1)">
+						
+					</view>
+				</view>
+			</view>
+			<view class="price-content">
+				<input type="text" value="" />
+				<text></text>
+				<input type="text" value="" />
+			</view>
+			<view class="ensured">
+				<view class="ensured-list" v-for="(ele, index) in ensured" :key="index" :class="{active:index == select2Index}" @click="changeSelect2(index)">
+					{{ele}}
+				</view>
+			</view>
+			<view class="authentication">
+				<view class="authentication-list" v-for="(ele, index) in ensured" :key="index" :class="{active:index == select3Index}" @click="changeSelect3(index)">
 					
 				</view>
 			</view>
@@ -18,61 +50,100 @@
 </template>
 
 <script>
-	import Site from '@/components/site/site.vue'
+	import liFilter from '@/components/Li-Filter/Li-Filter'
 	// import Site from './data.js'
 	export default {
 		components: {
-			Site
-		},
+			liFilter
+		},  
 		data() {
 			return {
+				serviceIndex: 0,
+				select1Index: ['', ''],
+				select2Index: '',
+				select3Index: 0,
+				datalist: {},
+				testList: {},
+				height: 0.6,
 				searchStr: '',
-				condition: [
-					{type: '服务', select_condition: [{type_name: '综合', selectArr: ['综合', '评价', '就觉得']}, {type_name: '小量', selectArr: []}]},
-					{type: '案例', select_condition: [{type_name: '热销', selectArr: ['狗', '鸡', '呀']}, {type_name: '小量', selectArr: []}]},
-					{type: '商铺', select_condition: [{type_name: '综合', selectArr: []}, {type_name: '成交', selectArr: []}, {type_name: '好评率', selectArr: []}]}
-				]
+				service: ['服务', '案例', '店铺'],
+				selectArr: [{type: '行业', arr: ['11', '22', '98', '88', '44', '11', '22', '98', '88', '11', '22', '98', '88', '11', '22', '98', '88', '11', '22', '98', '88']},
+				{type: '行业', arr: ['11', '22', '98', '88', '44', '11', '22', '98', '88', '11', '22', '98', '88', '11', '22', '98', '88', '11', '22', '98', '88']}],
+				ensured: ['888', '888', '999', '9055', '888', '888', '999', '9055', '888', '888', '999', '9055', '888', '888', '999', '9055',],
+				authentication: ['22', '44', '344']
 			}
 		},
-		methods: {
+		onLoad(){
+			let tempdata = {
+				filterType: 1,
+				data: []
+			};
+			let normalItem = {
+				title: '综合排序', //排序头的名称
+				value: 'a',
+				type: 1, //类型，0：没有下拉选项，1：单项下拉列表，2：多项列表，如地区选择
+				data: [
+					{
+						value: 1,
+						text: '综合排序'
+					},
+					{
+						value: 2,
+						text: '只看商家'
+					},
+					{
+						value: 3,
+						text: '只看产品'
+					}
+				]
+			};
 			
+			let sigleItem = {
+				title: '销量', //排序头的名称
+				value: 'b',
+				type: 0 //类型，0：没有下拉选项，1：单项下拉列表，2：多项列表，如地区选择
+			};
+			
+			let areaData = require('../../static/area.json');
+			let typeData = require('../../static/type.json');
+			
+			let AreaItem = {
+				title: '区域', //排序头的名称
+				value: 'c',
+				type: 2, //类型，0：没有下拉选项，1：单项下拉列表，2：多项列表，如地区选择
+				data: areaData
+			};
+			tempdata.data.push(AreaItem);
+			tempdata.data.push(normalItem);
+			tempdata.data.push(sigleItem);
+			this.datalist = tempdata;
+		},
+		methods: {
+			change(e) {
+				// console.log('----------change----------');
+				// console.log(e);
+			},
+			select(e) {
+				// console.log('----------select----------');
+				// console.log(e);
+			},
+			changeService(i){
+				this.serviceIndex = i
+			},
+			changeSelect1(i, j){
+				this.select1Index[i] = j
+			},
+			changeSelect2(i){
+				this.select2Index = i
+			},
+			changeSelect3(i){
+				this.select3Index = i
+			},
 		}
 	}
 </script>
 
 
 <style lang="scss" scoped>
-	.container{
-		position: relative;
-		padding: 0 30rpx;
-		background-color: #fafafa;
-		.search{
-			position: fixed;
-			box-sizing: border-box;
-			width: 100%;
-			left: 0;
-			top: 54px;
-			z-index: 10;
-			padding: 0 30rpx;
-			// margin-top: 20rpx;
-			.search-wrapper{
-				position: relative;
-				padding: 10rpx 0;
-				background-color: rgba(0, 0, 0, .1);
-				border-radius: 10rpx;
-				icon{
-					position: absolute;
-					left: 20rpx;
-					top: 50%;
-					transform: translateY(-50%);
-				}
-				input{
-					// height: 30px;
-					padding-left: 60rpx;
-					font-size: 12px;
-				}
-			}
-		}
-	}
-
+@import './home.scss'
 </style>
---
+++ src/pages/taskDetail/taskDetail.vue
@@ -0,0 +1,114 @@
+<template>
+	<view class="container">
+		<view class="task-mes">
+			<view class="swiper-box">
+				<swiper class="swiper" :indicator-dots="true" indicator-active-color="gray" :autoplay="false" :interval="1500"
+					:duration="500" :circular='true'>
+					<swiper-item>
+						<view class="swiper-item uni-bg-red">A</view>
+					</swiper-item>
+					<swiper-item>
+						<view class="swiper-item uni-bg-green">B</view>
+					</swiper-item>
+					<swiper-item>
+						<view class="swiper-item uni-bg-blue">C</view>
+					</swiper-item>
+				</swiper>
+			</view>
+			<view class="mes-box">
+				<view class="task-name">
+					logo谁设计
+				</view>
+				<view class="task-price">
+					<text>￥</text>8888887
+				</view>
+				<view class="add-mes">
+					<view class="add-mes-list1 add-mes-list">
+						<view class="num">
+							2
+						</view>
+						<view class="name">
+							开始时间
+						</view>
+					</view>
+					<view class="add-mes-list2 add-mes-list">
+						<view class="num">
+							5
+						</view>
+						<view class="name">
+							结束时间
+						</view>
+					</view>
+					<view class="add-mes-list3 add-mes-list">
+						<view class="num">
+							555
+						</view>
+						<view class="name">
+							tttt
+						</view>
+					</view>
+					<view class="add-mes-list4 add-mes-list">
+						<view class="num">
+							而二人
+						</view>
+						<view class="name">
+							哈弗uu
+						</view>
+					</view>
+				</view>
+			</view>
+		</view>
+		
+		<view class="user-info">
+			<view class="img-box">
+				<image src="../../static/imgs/u=708482407,3295795161&fm=26&gp=0.jpg" mode=""></image>
+			</view>
+			<view class="info-box">
+				<view class="user-name">
+					樱花岁月
+				</view>
+				<view class="task-level">
+					任务难度: 中等
+				</view>
+				<view class="site">
+					成都
+				</view>
+			</view>
+		</view>
+		<view class="comment">
+			<view class="title">
+				客户评价
+			</view>
+			<view class="comment-list">
+				<view class="user-info-box">
+					<view class="img-box">
+						<image src="../../static/imgs/u=708482407,3295795161&fm=26&gp=0.jpg" mode=""></image>
+					</view>
+					<view class="user-name">
+						8574777
+					</view>
+				</view>
+				<view class="user-comment">
+					十分满意
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			}
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+@import './taskDetail.scss';
+</style>
